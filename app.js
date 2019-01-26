bdcc393d6cccc7336e7788905c72a5239af624f7
Anand Muraleedharan <anand.muraleedharan@gmail.com>
2019-01-26 17:36:41 -0600

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ cameraTrigger.onclick = function() {
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
-    scanImage(cameraOutput);
+    scanImage(cameraOutput.src);
 };
 
 //Reading Text using OCRAD
